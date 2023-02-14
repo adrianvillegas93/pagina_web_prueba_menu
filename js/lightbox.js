c869272cae1bbd1d5816b43cc141cdb7d19e16b8
adrianvillegas93 <72955297+adrianvillegas93@users.noreply.github.com>
2023-02-14 13:48:31 -0600

    subir web site
    Se subieron todos los archivos

---
+++ js/lightbox.js
@@ -0,0 +1,23 @@
+const imagenes = document.querySelectorAll('.img_galeria');
+const imagenesLight = document.querySelector('.agregar_imagen');
+const contenedorLight = document.querySelector('.imagen_light');
+
+
+imagenes.forEach(imagen =>{
+    imagen.addEventListener('click', ()=>{
+        aparecerImagen(imagen.getAttribute('src'))
+    })
+})
+
+contenedorLight.addEventListener('click', (e)=>{
+    if(e.target != imagenesLight){
+        contenedorLight.classList.toggle('show')
+        imagenesLight.classList.toggle('showImage')
+    }
+})
+
+const aparecerImagen = (imagen)=>{
+    imagenesLight.src = imagen;
+    contenedorLight.classList.toggle('show')
+    imagenesLight.classList.toggle('showImage')
+}
